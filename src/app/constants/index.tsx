24d3f8863f5e414b12b2cdee4ac99fcaed41f028
ice-pattanachai <150659852+ice-pattanachai@users.noreply.github.com>
2024-05-22 09:25:05 +0700

    fix
    fix header+icon

---
+++ src/app/constants/index.tsx
@@ -0,0 +1,47 @@
+export const inclusions = [
+  {
+    title: 'Free Shipping',
+    description: 'Free shipping for order above $150',
+    icon: '/assets/icons-bootsteap/box-seam.svg',
+  },
+  {
+    title: 'Money Guarantee',
+    description: 'Within 30 days for an exchange',
+    icon: '/assets/icons-bootsteap/currency-dollar.svg',
+  },
+  {
+    title: 'Online Support',
+    description: '24 hours a day, 7 days a week',
+    icon: '/assets/icons-bootsteap/headset.svg.svg',
+  },
+  {
+    title: 'Flexible Payment',
+    description: 'Pay with multiple credit cards',
+    icon: '/assets/icons-bootsteap/credit-card-2-back.svg',
+  },
+]
+
+export const profileNavItems = [
+  {
+    title: 'Personal Information',
+    url: '/account',
+    icon: '/assets/icons-bootsteap/person.svg',
+  },
+  {
+    title: 'My Purchases',
+    url: '/account/purchases',
+    icon: '/assets/icons-bootsteap/credit-card-2-back.svg',
+  },
+  {
+    title: 'My Orders',
+    url: '/account/orders',
+    icon: '/assets/icons-bootsteap/box-seam.svg',
+  },
+  {
+    title: 'Logout',
+    url: '/logout',
+    icon: '/assets/icons-bootsteap/box-arrow-left.svg',
+  },
+]
+
+export const noHeaderFooterUrls = ['/create-account', '/login', '/recover-password']
